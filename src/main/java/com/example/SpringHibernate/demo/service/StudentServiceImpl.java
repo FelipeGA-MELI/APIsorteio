d3135dc4b1db6a236da0f4d27091f97f7fb8800a
Felipe Gusmao <FelipeGA-MELI>
2021-06-21 17:39:24 -0300

    API finalizada

--- src/main/java/com/example/SpringHibernate/demo/service/StudentServiceImpl.java
+++ src/main/java/com/example/SpringHibernate/demo/service/StudentServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.SpringHibernate.demo.service;
 import com.example.SpringHibernate.demo.model.Student;
 import com.example.SpringHibernate.demo.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -16,10 +18,15 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> getAlunos() {
-        List<Student>  studentList = new LinkedList<>();
+        Random random = new Random();
+        List<Student> studentList = studentRepository.findAll();
+        List<Student> selectedStudents = new LinkedList<>();
 
+        for(int i = 0; i < studentList.size(); i++) {
+            int randomIndex = random.nextInt(studentList.size());
+            selectedStudents.add(studentList.remove(randomIndex));
+        }
 
-
-        return studentRepository.findAll();
+        return selectedStudents;
     }
 }
